52ba7089c44b2dc849df390f01e6cac12252acb2
Backglass <cocok29@gmail.com>
2023-01-28 20:24:39 +0100

    Quitando todos los iconos menos los necesarios

--- enviar-formulario.php
+++ enviar-formulario.php
@@ -12,14 +12,28 @@
     //Asunto
     $subject = "Mensaje desde el formulario de contacto";
     
-    //Mensaje
-    $message = "Nombre: ".$nombre."\r\n";
-    $message .= "Email: ".$email."\r\n";
-    $message .= "Descripcion: ".$descripcion."\r\n";
-    $message .= "Mensaje: ".$mensaje."\r\n";
+    //Mensaje en formato HTML
+    $message = '
+    <html>
+        <head>
+            <title>Mensaje desde el formulario de contacto</title>
+        </head>
+        <body>
+            <h1>Mensaje desde el formulario de contacto</h1>
+            <p>Información enviada por el usuario:</p>
+            <ul>
+                <li><strong>Nombre:</strong> '.$nombre.'</li>
+                <li><strong>Email:</strong> '.$email.'</li>
+                <li><strong>Descripción:</strong> '.$descripcion.'</li>
+                <li><strong>Mensaje:</strong> '.$mensaje.'</li>
+            </ul>
+        </body>
+    </html>
+    ';
     
     //Cabeceras
-    $headers = 'Content-type: text/html; charset=UTF-8' . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= "From: ".$email."\r\n";
     $headers .= "Reply-To: ".$email."\r\n";
     $headers .= "X-Mailer: PHP/".phpversion();
@@ -27,8 +41,6 @@
     //Enviar el email
     if(mail($to,$subject,$message,$headers)){
         echo "Mensaje enviado correctamente";
-        
-        
     } else {
         echo "Error al enviar el mensaje";
     }
